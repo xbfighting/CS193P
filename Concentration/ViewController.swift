7e7d2c46d59249fcc490d82b538fe3e0dfa65f55
leicheng bai <xbfighting@hotmail.com>
2019-04-10 23:27:59 +0800

    Initial Commit

---
+++ Concentration/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by 白旭嘉 on 2019/4/10.
+//  Copyright © 2019 白旭嘉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
