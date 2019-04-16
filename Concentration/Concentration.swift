944cd0adbcda79642d8e44e11cc3e7c0861b48e4
leicheng bai <xbfighting@hotmail.com>
2019-04-17 00:01:31 +0800

    computable property part2 (reducing code, amazing language feature!)

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -12,28 +12,39 @@ class Concentration {
     
     var cards = [Card]()
     
-    var indexOfOneAndOnluFaceUpCard: Int?
+    var indexOfOneAndOnluFaceUpCard: Int? {
+        get {
+            var foundIndex: Int?
+            for index in cards.indices {
+                if cards[index].isFaceUp {
+                    if foundIndex == nil {
+                        foundIndex = index
+                    } else {
+                        return nil
+                    }
+                }
+            }
+            return foundIndex
+        }
+        set {
+            for index in cards.indices {
+                cards[index].isFaceUp = (index == newValue)
+            }
+        }
+    }
     
     func chooseCard(at index: Int) {
         if !cards[index].isMatched {
             if let matchIndex = indexOfOneAndOnluFaceUpCard, matchIndex != index {
                 // check if cards match
                 if cards[matchIndex].identifier == cards[index].identifier {
-                    cards[index].isMatched = true
                     cards[matchIndex].isMatched = true
+                    cards[index].isMatched = true
                 }
                 cards[index].isFaceUp = true
-                indexOfOneAndOnluFaceUpCard = nil
             } else {
-                // either no cards or 2 cards are face up
-                for flipDownIndex in cards.indices {
-                    cards[flipDownIndex].isFaceUp = false
-                }
-                
-                cards[index].isFaceUp = true
                 indexOfOneAndOnluFaceUpCard = index
             }
-            
         }
     }
     
