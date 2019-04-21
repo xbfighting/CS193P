63628dc9e0a4638ea3ee3b0809cda33822e0224e
leicheng bai <xbfighting@hotmail.com>
2019-04-21 23:08:33 +0800

    String.Index

--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -61,13 +61,15 @@ class ViewController: UIViewController {
         }
     }
     
-    private var emojiChoices = ["👻","🎃","🥦", "🧶", "🎮", "💸", "🔋", "🥎", "🎱"]
+//    private var emojiChoices = ["👻","🎃","🥦", "🧶", "🎮", "💸", "🔋", "🥎", "🎱"]
+    private var emojiChoices = "👻🎃🥦🧶🎮💸🔋🥎🎱"
     
     private var emoji = [Card: String]()
     
     private func emoji(for card: Card) -> String {
         if emoji[card] == nil, emojiChoices.count > 0 {
-            emoji[card] = emojiChoices.remove(at: emojiChoices.count.arc4random)
+            let randomStringIndex = emojiChoices.index(emojiChoices.startIndex, offsetBy: emojiChoices.count.arc4random)
+            emoji[card] = String(emojiChoices.remove(at: randomStringIndex))
         }
         return emoji[card] ?? "✕"
     }
